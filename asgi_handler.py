d07abaf2cbd896b528ed798c126c2ed3a079c4cc
DevTeo1622 <gaviriacardonamateo1@gmail.com>
2025-10-29 07:08:24 -0500

    deploy

--- asgi_handler.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from main import app  # importa tu app FastAPI
-
-# Vercel necesita que el archivo exponga una variable llamada `handler`
-# que sea una función ASGI compatible
-handler = app
